1718298cf853bf7c7d170b149660da7713b69265
dnotle <dnotle@gmail.com>
2021-04-01 18:22:33 -0400

    Create sample.js

---
+++ sample.js
@@ -0,0 +1,12 @@
+{  
+	"employee1": {  
+		"name":       "sonoo",   
+		"salary":      56000,   
+		"married":    true  
+	},
+	"employee2": {  
+		"name":       "sjoo",   
+		"salary":      500,   
+		"married":    false 
+	}  
+} 
